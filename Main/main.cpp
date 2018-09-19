3d54dbd023a524b04e029898dfa7f1e0d19f70e6
Grace Love <grlove@graces-mbp.domain>
2018-09-18 20:59:52 -0600

    test

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,15 +1,54 @@
-//
-//  main.cpp
-//  Main
-//
-//  Created by Grace Love on 9/18/18.
-//  Copyright © 2018 Grace Love. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main(){
+
+char userInput;
+bool menuOn = true;
+    
+while(menuOn != false){
+cout << endl;
+cout << "a. count by ones\n";
+cout << "b. count by twos\n";
+cout << "e. exit\n";
+
+cin >> userInput;
+
+switch (userInput){
+  case 'a':
+    for(int countA=1; countA <=10; ++countA)
+    {
+      cout << countA << " ";
+    }
+        break;
+    case 'A':
+        for(int countA=1; countA <=10; ++countA)
+        {
+            cout << countA << " ";
+        }
+        break;
+  case 'b':
+    for(int countB = 2; countB<= 20; countB = countB + 2)
+    {
+      cout << countB << " ";
+    }
+        break;
+    case 'B':
+        for(int countB = 2; countB<= 20; countB = countB + 2)
+        {
+            cout << countB << " ";
+        }
+        break;
+  case 'e':
+        menuOn = false;
+        break;
+    case 'E':
+        menuOn = false;
+        break;
+  default:
+    cout << "Please choose a, b, or e: \n";
+        break;
+}
+}
+  return 0;
 }
