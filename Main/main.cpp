da0fc2a85ae3ce00abc9e7a1289ddb486c1f0cf0
Grace Love <grlove@graces-mbp.domain>
2018-09-18 20:21:23 -0600

    Initial Commit

---
+++ Main/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Main
+//
+//  Created by Grace Love on 9/18/18.
+//  Copyright © 2018 Grace Love. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
